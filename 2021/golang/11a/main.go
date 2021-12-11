04963da01fabeccb3479f740009ca78a0d9f0ae6
Damien Dart <damiendart@pobox.com>
2021-12-11 08:34:28 +0000

    Add Go solutions for AoC 2021 Day 11.

---
+++ 2021/golang/11a/main.go
@@ -0,0 +1,114 @@
+// This file was written by Damien Dart, <damiendart@pobox.com>. This is
+// free and unencumbered software released into the public domain. For
+// more information, please refer to the accompanying "UNLICENCE file.
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func calculateFlashes(input [][]int, steps int) int {
+	var flashes int
+
+	for step := 0; step < steps; step++ {
+		for i, line := range input {
+			for j := range line {
+				input[i][j]++
+			}
+		}
+
+		flashing := true
+
+		for flashing {
+			for i, line := range input {
+				for j := range line {
+					if input[i][j] > 9 {
+						flashes++
+
+						if j > 0 && input[i][j-1] != 0 {
+							input[i][j-1]++
+						}
+
+						if j < len(line)-1 && input[i][j+1] != 0 {
+							input[i][j+1]++
+						}
+
+						if i > 0 && input[i-1][j] != 0 {
+							input[i-1][j]++
+						}
+
+						if i < len(input)-1 && input[i+1][j] != 0 {
+							input[i+1][j]++
+						}
+
+						if i > 0 && j > 0 && input[i-1][j-1] != 0 {
+							input[i-1][j-1]++
+						}
+
+						if i > 0 && j < len(line)-1 && input[i-1][j+1] != 0 {
+							input[i-1][j+1]++
+						}
+
+						if i < len(input)-1 && j > 0 && input[i+1][j-1] != 0 {
+							input[i+1][j-1]++
+						}
+
+						if i < len(input)-1 && j < len(line)-1 && input[i+1][j+1] != 0 {
+							input[i+1][j+1]++
+						}
+
+						input[i][j] = 0
+					}
+				}
+			}
+
+			flashing = false
+
+			for i, line := range input {
+				for j := range line {
+					if input[i][j] > 9 {
+						flashing = true
+					}
+				}
+			}
+		}
+	}
+
+	return flashes
+}
+
+func main() {
+	var input [][]int
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for scanner.Scan() {
+		var lineValues []int
+
+		line := scanner.Text()
+
+		for _, item := range strings.Split(line, "") {
+			number, err := strconv.Atoi(item)
+
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			lineValues = append(lineValues, number)
+		}
+
+		input = append(input, lineValues)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(calculateFlashes(input, 100))
+}
---
+++ 2021/golang/11b/main_test.go
@@ -0,0 +1,26 @@
+// This file was written by Damien Dart, <damiendart@pobox.com>. This is
+// free and unencumbered software released into the public domain. For
+// more information, please refer to the accompanying "UNLICENCE file.
+
+package main
+
+import "testing"
+
+func TestCalculateFlashes(t *testing.T) {
+	input := [][]int{
+		[]int{5, 4, 8, 3, 1, 4, 3, 2, 2, 3},
+		[]int{2, 7, 4, 5, 8, 5, 4, 7, 1, 1},
+		[]int{5, 2, 6, 4, 5, 5, 6, 1, 7, 3},
+		[]int{6, 1, 4, 1, 3, 3, 6, 1, 4, 6},
+		[]int{6, 3, 5, 7, 3, 8, 5, 4, 7, 8},
+		[]int{4, 1, 6, 7, 5, 2, 4, 6, 4, 5},
+		[]int{2, 1, 7, 6, 8, 4, 1, 7, 2, 1},
+		[]int{6, 8, 8, 2, 8, 8, 1, 1, 3, 4},
+		[]int{4, 8, 4, 6, 8, 4, 8, 5, 5, 4},
+		[]int{5, 2, 8, 3, 7, 5, 1, 5, 2, 6},
+	}
+
+	if output := calculateSynchronisedStep(input); output != 195 {
+		t.Errorf("Expected 195, got %#v", output)
+	}
+}
